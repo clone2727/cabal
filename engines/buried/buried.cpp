scummvm/scummvm

BURIED: Begin to load the Arial font
TrueType2 is now required for the engine

engines/buried/buried.cpp
@@ -22,8 +22,14 @@
 
 #include "common/scummsys.h"
 #include "common/error.h"
+#include "common/fs.h"
+#ifdef MACOSX
+#include "common/macresman.h"
+#endif
 #include "common/system.h"
 #include "common/textconsole.h"
+#include "graphics/font.h"
+#include "graphics/fonts/ttf.h"
 
 #include "buried/buried.h"
 #include "buried/database.h"
@@ -61,6 +67,66 @@ Common::Error BuriedEngine::run() {
 		error("Failed to load library DLL '%s'", getLibraryName().c_str());
 
 	return Common::kNoError;
+}
+
+Graphics::Font *BuriedEngine::createFont(int size) const {
+	Common::SeekableReadStream *stream = 0;
+
+	// HACK: Try to load the system font
+	// TODO: MS Gothic for the Japanese version (please buy for clone2727)
+	// Arial for everything else (???)
+#if defined(WIN32)
+	Common::FSNode fontPath("C:/WINDOWS/Fonts/arial.ttf");
+
+	if (fontPath.exists() && !fontPath.isDirectory() && fontPath.isReadable())
+		stream = fontPath.createReadStream();
+
+	if (!stream) {
+		Common::FSNode win2kFontPath("C:/WINNT/Fonts/arial.ttf");
+
+		if (win2kFontPath.exists() && !win2kFontPath.isDirectory() && win2kFontPath.isReadable())
+			stream = win2kFontPath.createReadStream();
+	}
+#elif defined(MACOSX)
+	// Attempt to load the font from the Arial.ttf font first
+	Common::FSNode fontPath("/Library/Fonts/Arial.ttf");
+
+	if (fontPath.exists() && !fontPath.isDirectory() && fontPath.isReadable())
+		stream = fontPath.createReadStream();
+
+	if (!stream) {
+		// Try the suitcase on the system
+		Common::FSNode fontDirectory("/Library/Fonts");
+		Common::MacResManager resFork;
+
+		// DOUBLE HACK WARNING: Just assume it's 0x1000
+		// (it should always be this, the first font, but parsing the FOND would be better)
+		if (fontDirectory.exists() && fontDirectory.isDirectory() && resFork.open(fontPath, "Arial") && resFork.hasResFork())
+			stream = resFork.getResource(MKTAG('s', 'f', 'n', 't'), 0x1000);
+
+		// ...and one last try
+		if (!stream) {
+			Common::FSNode msFontDirectory("/Library/Fonts/Microsoft");
+			if (fontDirectory.exists() && fontDirectory.isDirectory() && resFork.open(fontPath, "Arial") && resFork.hasResFork())
+				stream = resFork.getResource(MKTAG('s', 'f', 'n', 't'), 0x1000);
+		}
+	}
+#endif
+
+	if (!stream) {
+		// TODO: Try to load an equivalent font from the theme
+		return 0;
+	}
+
+	// TODO: Make the monochrome mode optional
+	// Win3.1 obviously only had raster fonts, but BIT Win3.1 will render
+	// with the TrueType font on Win7/Win8 (at least)
+	// TODO: The mapping is code page 1252, but it should be 1:1 to Unicode
+	// for the characters we need.
+	// TODO: shift-jis (code page 932) for the Japanese version (again, buy for clone2727)
+	Graphics::Font *font = Graphics::loadTTFFont(*stream, size, 0, Graphics::kTTFRenderModeMonochrome, 0);
+	delete stream;
+	return font;
 }
 
 } // End of namespace Buried

clone2727/cabal

engines/buried/buried.cpp
@@ -22,8 +22,14 @@
 
 #include "common/scummsys.h"
 #include "common/error.h"
+#include "common/fs.h"
+#ifdef MACOSX
+#include "common/macresman.h"
+#endif
 #include "common/system.h"
 #include "common/textconsole.h"
+#include "graphics/font.h"
+#include "graphics/fonts/ttf.h"
 
 #include "buried/buried.h"
 #include "buried/database.h"
@@ -61,6 +67,66 @@ Common::Error BuriedEngine::run() {
 		error("Failed to load library DLL '%s'", getLibraryName().c_str());
 
 	return Common::kNoError;
+}
+
+Graphics::Font *BuriedEngine::createFont(int size) const {
+	Common::SeekableReadStream *stream = 0;
+
+	// HACK: Try to load the system font
+	// TODO: MS Gothic for the Japanese version (please buy for clone2727)
+	// Arial for everything else (???)
+#if defined(WIN32)
+	Common::FSNode fontPath("C:/WINDOWS/Fonts/arial.ttf");
+
+	if (fontPath.exists() && !fontPath.isDirectory() && fontPath.isReadable())
+		stream = fontPath.createReadStream();
+
+	if (!stream) {
+		Common::FSNode win2kFontPath("C:/WINNT/Fonts/arial.ttf");
+
+		if (win2kFontPath.exists() && !win2kFontPath.isDirectory() && win2kFontPath.isReadable())
+			stream = win2kFontPath.createReadStream();
+	}
+#elif defined(MACOSX)
+	// Attempt to load the font from the Arial.ttf font first
+	Common::FSNode fontPath("/Library/Fonts/Arial.ttf");
+
+	if (fontPath.exists() && !fontPath.isDirectory() && fontPath.isReadable())
+		stream = fontPath.createReadStream();
+
+	if (!stream) {
+		// Try the suitcase on the system
+		Common::FSNode fontDirectory("/Library/Fonts");
+		Common::MacResManager resFork;
+
+		// DOUBLE HACK WARNING: Just assume it's 0x1000
+		// (it should always be this, the first font, but parsing the FOND would be better)
+		if (fontDirectory.exists() && fontDirectory.isDirectory() && resFork.open(fontPath, "Arial") && resFork.hasResFork())
+			stream = resFork.getResource(MKTAG('s', 'f', 'n', 't'), 0x1000);
+
+		// ...and one last try
+		if (!stream) {
+			Common::FSNode msFontDirectory("/Library/Fonts/Microsoft");
+			if (fontDirectory.exists() && fontDirectory.isDirectory() && resFork.open(fontPath, "Arial") && resFork.hasResFork())
+				stream = resFork.getResource(MKTAG('s', 'f', 'n', 't'), 0x1000);
+		}
+	}
+#endif
+
+	if (!stream) {
+		// TODO: Try to load an equivalent font from the theme
+		return 0;
+	}
+
+	// TODO: Make the monochrome mode optional
+	// Win3.1 obviously only had raster fonts, but BIT Win3.1 will render
+	// with the TrueType font on Win7/Win8 (at least)
+	// TODO: The mapping is code page 1252, but it should be 1:1 to Unicode
+	// for the characters we need.
+	// TODO: shift-jis (code page 932) for the Japanese version (again, buy for clone2727)
+	Graphics::Font *font = Graphics::loadTTFFont(*stream, size, 0, Graphics::kFontRenderMonochrome, 0);
+	delete stream;
+	return font;
 }
 
 } // End of namespace Buried